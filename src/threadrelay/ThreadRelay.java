7c05aebd7444bcb8dd70846db6e338b6a83928c0
riccardomontedori <montedori.riccardo@franchettisalviani.net>
2026-04-21 10:52:56 +0200

    Implementazione Parte Grafica
    Sviluppo della grafica da codice tramite IA

--- src/threadrelay/ThreadRelay.java
+++ src/threadrelay/ThreadRelay.java
@@ -15,6 +15,7 @@ public class ThreadRelay {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new InterfacciaAtleta().setVisible(true);
     }
     
 }
