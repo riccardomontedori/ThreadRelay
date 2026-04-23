580a10c697ec6da70c6cf6cb6d10783ee9d57624
riccardomontedori <montedori.riccardo@franchettisalviani.net>
2026-04-23 18:16:11 +0200

    Implementazione Parte Logica
    Completamento delle classi Atleta e GestoreGara insieme ai metodi da richiamare nel form

--- src/threadrelay/Atleta.java
+++ src/threadrelay/Atleta.java
@@ -8,6 +8,45 @@ package threadrelay;
  *
  * @author montedori.riccardo
  */
-public class Atleta {
-    
+public class Atleta extends Thread {
+    public int id;
+    public int conteggio = 0;
+    public int velocita;
+    public boolean inEsecuzione = true;
+    public boolean inPausa = false;
+    private Atleta prossimoAtleta;
+    private GestoreGara gestore;
+
+    public Atleta(int id, int velocita, GestoreGara gestore) {
+        this.id = id;
+        this.velocita = velocita;
+        this.gestore = gestore;
+    }
+
+    public void setProssimoAtleta(Atleta prossimo) {
+        this.prossimoAtleta = prossimo;
+    }
+
+    @Override
+    public void run() {
+        while (conteggio < 100 && inEsecuzione) {
+            if (!inPausa) {
+                if (conteggio == 90 && prossimoAtleta != null) {
+                    prossimoAtleta.start();
+                }
+
+                conteggio++;
+                
+                try {
+                    Thread.sleep(velocita);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            } else {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {}
+            }
+        }
+    }
 }
--- src/threadrelay/GestoreGara.java
+++ src/threadrelay/GestoreGara.java
@@ -9,5 +9,31 @@ package threadrelay;
  * @author montedori.riccardo
  */
 public class GestoreGara {
-    
+    public Atleta[] atleti = new Atleta[4];
+
+    public void preparaGara(int msVelocita) {
+        for (int i = 0; i < 4; i++) {
+            atleti[i] = new Atleta(i + 1, msVelocita, this);
+        }
+        
+        atleti[0].setProssimoAtleta(atleti[1]);
+        atleti[1].setProssimoAtleta(atleti[2]);
+        atleti[2].setProssimoAtleta(atleti[3]);
+    }
+
+    public void avvia() {
+        if (atleti[0] != null) atleti[0].start();
+    }
+
+    public void pausa(boolean statoPausa) {
+        for (Atleta a : atleti) {
+            if (a != null) a.inPausa = statoPausa;
+        }
+    }
+
+    public void stop() {
+        for (Atleta a : atleti) {
+            if (a != null) a.inEsecuzione = false;
+        }
+    }
 }
